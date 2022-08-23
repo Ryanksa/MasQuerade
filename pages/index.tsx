07d38efd4add5e5f5922b33d3f330f100f13b7e1
Ryanksa <ryanlin58@gmail.com>
2022-08-23 19:12:38 -0400

    Added simple preview on landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,108 @@
 import type { NextPage } from "next";
+import React, { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 import { getServerSidePropsAuth } from "../utils/auth";
+import { generateRandomString } from "../utils/general";
 import UnauthenticatedLayout from "../layouts/UnauthenticatedLayout";
+import MasquerText from "../components/MasquerText";
+import Phone from "../components/Phone";
+import ChatMessage from "../components/ChatMessage";
+import { ChatMessage as ChatMessageType } from "../models/chat";
+
+const DEMO_ROOM_ID = generateRandomString(32);
+const DEMO_USERNAME = generateRandomString(32);
+const MY_USERNAME = generateRandomString(32);
 
 const Home: NextPage = () => {
+  const [messages, setMessages] = useState<ChatMessageType[]>([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    setMessages([
+      {
+        id: generateRandomString(32),
+        username: DEMO_USERNAME,
+        name: "Rewn",
+        roomId: DEMO_ROOM_ID,
+        room: "Demo",
+        content: "Hi there!",
+        postedOn: new Date().toISOString(),
+      },
+    ]);
+  }, []);
+
+  const handleSend = () => {
+    setMessages((prevMessages) => [
+      {
+        id: generateRandomString(32),
+        username: MY_USERNAME,
+        name: "Me",
+        roomId: DEMO_ROOM_ID,
+        room: "Demo",
+        content: message,
+        postedOn: new Date().toISOString(),
+      },
+      ...prevMessages,
+    ]);
+    setMessage("");
+  };
+
   return (
     <UnauthenticatedLayout>
-      <></>
+      <div className="hidden relative sm:block">
+        <div className="absolute left-1/2 -top-6 z-10">
+          <MasquerText
+            text="PreVieW"
+            flipIndices={[1, 3]}
+            leftFontSize={72}
+            fontStepSize={-4}
+            transform="rotate(12deg)"
+            transformOrigin=""
+            hoverInvert={false}
+          />
+        </div>
+        <Phone>
+          <div className="relative w-full h-full overflow-hidden flex flex-col overflow-anchor-none">
+            <div className="w-full h-full pb-8 flex flex-col-reverse overflow-scroll scrollbar-hidden">
+              {messages.map((msg) => (
+                <div key={msg.id} className="mt-8">
+                  <ChatMessage
+                    message={msg}
+                    received={msg.username === DEMO_USERNAME}
+                    enterAnimation={true}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="w-full flex items-center p-[8px]">
+              <input
+                type="text"
+                className="w-10/12 text-[24px] p-[4px] border-[3px] border-neutral-900 rounded-[5px]"
+                placeholder="Send a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") handleSend();
+                }}
+              />
+              <div
+                className="cursor-pointer absolute right-2"
+                onClick={handleSend}
+              >
+                <MasquerText
+                  text="Send"
+                  flipIndices={[]}
+                  leftFontSize={42}
+                  fontStepSize={0}
+                  transform="rotate(-8deg)"
+                  transformOrigin=""
+                  hoverInvert={true}
+                />
+              </div>
+            </div>
+          </div>
+        </Phone>
+      </div>
     </UnauthenticatedLayout>
   );
 };
--- utils/general.ts
+++ utils/general.ts
@@ -6,6 +6,16 @@ export const getRandomArbitrary = (min: number, max: number) => {
   return Math.random() * (max - min) + min;
 };
 
+export const generateRandomString = (length: number) => {
+  let result = "";
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
 export const incrementSocialStats = () => {
   return getRandomArbitrary(0, 3);
 };
