7dd980a6e9f8d11b2d783ef4d185cb97c8d4fe04
Ryanksa <ryanlin58@gmail.com>
2022-09-03 10:34:56 -0400

    Added custom favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ function Document() {
   return (
     <Html>
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,700&family=KoHo:wght@600&display=swap&family=Gowun+Dodum&display=swap"
           rel="stylesheet"
