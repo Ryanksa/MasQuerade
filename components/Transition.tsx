c9a9c12811ef63a4ba9f8600a810372a3e5d1266
Ryanksa <ryanlin58@gmail.com>
2022-11-11 18:03:26 -0500

    Updated page nav transition effect to go from top to bottom

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -22,19 +22,19 @@ function Transition() {
         <div className="fixed left-0 top-0 w-screen h-screen overflow-hidden z-40">
           <div
             className={`
-              absolute left-0 bottom-0 w-0 h-[120%] -skew-y-[15deg]
+              absolute right-[45%] -top-[39%] w-0 h-[45%] -skew-y-[15deg]
               ${styles.bar} ${styles.bar1}
             `}
           />
           <div
             className={`
-              absolute right-0 -bottom-[30vh] w-0 h-[45%] -skew-y-[9deg] 
+              absolute left-0 bottom-0 w-0 h-[120%] -skew-y-[15deg]
               ${styles.bar} ${styles.bar2}
             `}
           />
           <div
             className={`
-              absolute left-0 -top-[30vh] w-0 h-[45%] -skew-y-[15deg] 
+              absolute right-0 -bottom-[15%] w-0 h-[45%] -skew-y-[9deg]   
               ${styles.bar} ${styles.bar3}
             `}
           />
